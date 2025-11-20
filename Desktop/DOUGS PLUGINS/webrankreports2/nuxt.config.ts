dc1266c27c9d731425374d99880047c7a44615cf
Virtual Market Advantage <admin@vma.agency>
2025-11-20 15:34:18 -0600

    Fix Netlify build: add Nitro preset and correct @netlify/functions version

---
+++ Desktop/DOUGS PLUGINS/webrankreports2/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-01-01',
+  devtools: { enabled: true },
+  
+  modules: ['@nuxtjs/tailwindcss'],
+  
+  css: ['~/assets/css/main.css'],
+  
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL || '',
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY || ''
+    }
+  },
+  
+  nitro: {
+    preset: 'netlify'
+  },
+  
+  ssr: true
+})
+
