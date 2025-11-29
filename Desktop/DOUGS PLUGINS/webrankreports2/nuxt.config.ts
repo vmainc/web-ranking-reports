4a1342da62dacaba6749204c5a68c7d57c78f630
Virtual Market Advantage <admin@vma.agency>
2025-11-29 11:26:44 -0600

    Fix: Disable devtools in production builds

--- Desktop/DOUGS PLUGINS/webrankreports2/nuxt.config.ts
+++ Desktop/DOUGS PLUGINS/webrankreports2/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-01-01',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
   
   modules: ['@nuxtjs/tailwindcss'],
   
